a8da1ac1fa0a608070f536996b28025ddcbc1130
Emil Muhitdinov <laizybot@gmail.com>
2024-10-17 00:17:10 +0300

    Add session to services as argument

--- app/services.py
+++ app/services.py
@@ -1,6 +1,7 @@
 import datetime
 
 from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import SportActivity, Boys
 from app.db import async_session
@@ -10,51 +11,55 @@ from app.settings import get_settings
 
 class BoysService:
     @staticmethod
-    async def add_boys() -> None:
+    async def add_boys(session: AsyncSession) -> None:
         boys = get_settings().boys
 
-        async with async_session() as session:
-            for boy in boys:
-                boy_row = (await session.execute(
-                    select(Boys).where(Boys.tg_username == boy["tg_username"])
-                )).scalars().first()
+        for boy in boys:
+            boy_row = (await session.execute(
+                select(Boys).where(Boys.tg_username == boy["tg_username"])
+            )).scalars().first()
 
-                if boy_row is not None:
-                    continue
+            if boy_row is not None:
+                continue
 
-                session.add(Boys(
-                    name=boy["name"],
-                    call_sign=boy["call_sign"],
-                    tg_username=boy["tg_username"]
-                ))
-            await session.commit()
+            session.add(Boys(
+                name=boy["name"],
+                call_sign=boy["call_sign"],
+                tg_username=boy["tg_username"]
+            ))
+        await session.commit()
+
+    @staticmethod
+    async def get_boy(session: AsyncSession, tg_username: str):
+        boy_query = select(Boys).where(Boys.tg_username == tg_username)
+        return (await session.execute(boy_query)).scalars().first()
 
 
 class SportService:
-    async def add_report(self,
-                         username: str,
-                         date: datetime.date) -> None:
-        async with async_session() as session:
-            boy_request = select(Boys).where(Boys.tg_username == (username))
-            boy = await session.execute(boy_request)
+    @staticmethod
+    async def add_report(session: AsyncSession, tg_username: str, username: str, date: datetime.date) -> None:
+        boy_request = select(Boys).where(Boys.tg_username == (username))
+        boy = await session.execute(boy_request)
 
-            report = SportActivity(boy=boy, date=date)
+        report = SportActivity(boy=boy, date=date)
 
-            session.add(report)
-            await session.commit()
+        session.add(report)
+        await session.commit()
 
-    async def get_week_stats(self):
+    @staticmethod
+    async def get_week_stats(session: AsyncSession):
         week = datetime.date.today().isocalendar()[1]
 
-        async with async_session() as session:
-            week_reports = select(SportActivity).\
-                where(SportActivity.report_week == week).\
-                join(SportActivity.boy)
+        week_reports = select(SportActivity).\
+            where(SportActivity.report_week == week).\
+            join(SportActivity.boy)
 
-            query = select(
-                Boys.tg_username,
-                Boys.name,
-                func.count(Boys.id).label("week_reports_count")
-            ).from_statement(week_reports)
+        query = select(
+            Boys.tg_username,
+            Boys.name,
+            func.count(Boys.id).label("week_reports_count")
+        ).from_statement(week_reports)
 
-            await session.execute(query)
+        week_stats = (await session.execute(query)).scalars()
+
+        return week_stats
