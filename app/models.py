9ef62668ca35dfdbddd85dc6cfc483b7ae88e4ab
Emil Muhitdinov <laizybot@gmail.com>
2024-10-16 23:20:03 +0300

    Add boys model. Modify SportActivity model.

--- app/models.py
+++ app/models.py
@@ -1,21 +1,28 @@
 from datetime import datetime, date
-from enum import Enum
 
-from sqlalchemy import func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from app.db import Base
 
 
-class Boys(Enum):
-    KIRILL = 1
-    LENYA = 2
-    EMIL = 3
+class Boys(Base):
+    __tablename__ = "Boys"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    call_sign: Mapped[str]
+    tg_username: Mapped[str]
+    sport_activity: Mapped[list["SportActivity"]] = relationship()
 
 
 class SportActivity(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[Boys]
-    sport_date: Mapped[date]
+    __tablename__ = "SportActivity"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    boy: Mapped["Boys"] = mapped_column(ForeignKey("Boys.id"))
+    report_date: Mapped[date]
+    report_week: Mapped[int]
     create_date: Mapped[datetime] = mapped_column(server_default=func.now())
