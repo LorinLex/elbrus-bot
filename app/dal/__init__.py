059ee5a8db6051e36468fc97f0563f1787388f45
Emil Muhitdinov <laizybot@gmail.com>
2024-11-12 01:04:33 +0300

    Rename dal file

--- app/dal/__init__.py
+++ app/dal/__init__.py
@@ -1,5 +1,5 @@
 from .boys import Boy, get_boy, add_boys
-from .sport_activity import add_report, get_month_stats, get_week_stats
+from .sport import add_report, get_month_stats, get_week_stats
 
 __all__ = [
     "Boy",
