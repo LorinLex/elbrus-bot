fca1ce09356e20f5944d22b783ca9e0e9a6a5d01
Emil Muhitdinov <laizybot@gmail.com>
2024-11-12 00:52:58 +0300

    Split dal.py to different files

---
+++ app/dal/__init__.py
@@ -0,0 +1,11 @@
+from .boys import Boy, get_boy, add_boys
+from .sport_activity import add_report, get_month_stats, get_week_stats
+
+__all__ = [
+    "Boy",
+    "get_boy",
+    "add_boys",
+    "add_report",
+    "get_month_stats",
+    "get_week_stats"
+]
--- app/dal/sport_activity.py
+++ app/dal/sport_activity.py
@@ -7,58 +7,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import db_manager
 from app.models import SportActivityModel, BoyModel
 
-from app.settings import get_settings
-
-
-@dataclass
-class Boy:
-    name: str
-    call_sign: str
-    tg_username: str
-
-    @classmethod
-    def from_orm(cls, model: BoyModel) -> 'Boy':
-        return cls(
-            name=model.name,
-            call_sign=model.call_sign,
-            tg_username=model.tg_username
-        )
-
 
 @dataclass
 class SportStats:
     call_sign: str
     sum_distance: float
     reports_count: int
-
-
-@db_manager.connection
-async def add_boys(session: AsyncSession) -> None:
-    boys = get_settings().boys
-
-    for boy in boys:
-        boy_row = (await session.execute(
-            select(BoyModel).where(BoyModel.tg_username == boy["tg_username"])
-        )).scalars().first()
-
-        if boy_row is not None:
-            continue
-
-        session.add(BoyModel(
-            name=boy["name"],
-            call_sign=boy["call_sign"],
-            tg_username=boy["tg_username"]
-        ))
-    await session.commit()
-
-
-@db_manager.connection
-async def get_boy(session: AsyncSession, tg_username: str) -> Boy | None:
-    boy_query = select(BoyModel).where(BoyModel.tg_username == tg_username)
-    row = (await session.execute(boy_query)).first()
-    if row is None:
-        return None
-    return Boy.from_orm(row[0])
 
 
 @db_manager.connection
